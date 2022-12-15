b8cd36b8f6cc62d53c7f6b7f77d0c2ee1e6c36ce
freak906 <119906147+freak906@users.noreply.github.com>
2022-12-15 11:42:27 -0700

    add pokemons
    cleaned some extra code and added 3 pokemons in an array with some objects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-alert('Hello World');
-favoriteFood = 'Chips';
-document.write(favoriteFood);
+let pokemonList = [ {
+    name: 'Scyther',
+    height: 1.5,
+    types: ['bug', 'flying', 'swarm']
+},{
+    name: 'Mewtwo',
+    height: 2,
+    types: ['psychic', 'pressure', 'unnerve']
+},{
+    name: 'Noctowl',
+    height: 1.6,
+    types: ['flying', 'insomnia', 'keen-eye']
+}];
