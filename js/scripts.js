3543bf3f6bf9b9e1dde8cc130aaea1da9bdc3148
freak906 <119906147+freak906@users.noreply.github.com>
2022-12-16 12:13:36 -0700

    added loop
    created a loop to get a pokemon's info in order

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,11 @@ let pokemonList = [ {
     height: 1.6,
     types: ['flying', 'insomnia', 'keen-eye']
 }];
+let big = " - Wow, that's big!";
+for (let i = 0; i < pokemonList.length; i++){
+    if (pokemonList[i].height > 1.6){
+        document.write(pokemonList[i].name + ' ' + '(height: ' + pokemonList[i].height + ')' + big + '<br>' );
+    }else{
+        document.write(pokemonList[i].name + ' ' + '(height: ' + pokemonList[i].height + ')' + '<br>');
+    }
+}
