a555f166fb892b6e00670f69d564d1f8546d9237
freak906 <119906147+freak906@users.noreply.github.com>
2022-12-28 13:54:42 -0700

    added DOM for pokemons to display
    used DOM to display pokemon's buttons and log their info when click it

--- js/scripts.js
+++ js/scripts.js
@@ -15,9 +15,9 @@ let pokemonList = [{
     type: 'flying',
     weaknesses: ['ice','rock','electric']
 }];
-    function add(item) {
-        if (typeof item === "object" && item.name && item.height && item.type && item.weaknesses && Object.keys(item).length <= 4){
-            pokemonList.push(item);
+    function add(pokemon) {
+        if (typeof pokemon === "object" && pokemon.name && pokemon.height && pokemon.type && pokemon.weaknesses && Object.keys(pokemon).length <= 4){
+            pokemonList.push(pokemon);
         }else{
             console.log('Make sure item is an object and has name, height, type and weaknesses properties');
         }
@@ -25,18 +25,32 @@ let pokemonList = [{
     function getAll(){
         return pokemonList;
     }
+    function addListItem(pokemon){
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('pokemon-button');
+        listItem.appendChild(button);
+        pokemonList.appendChild(listItem);
+        button.addEventListener('click', function(event){
+            showDetails(pokemon);
+        });
+    }
+    function showDetails(pokemon){
+        console.log(pokemon);
+    }
     return {
         getAll: getAll,
-        add: add
+        add: add,
+        addListItem: addListItem
     };
 })();
-
+//pushed a new pokemon in
 let newPokemon = {name: 'Ninetales', height: 3.7, type: 'fire', weaknesses: ['water','ground','rock']};
 console.log(pokemonRepository.getAll());
 console.log(pokemonRepository.add(newPokemon));
 
-pokemonRepository.getAll().forEach(function(pokemons) {
-    document.write('Name: ' + pokemons.name + ', ' + 'Height: '  + pokemons.height + ', ' + 'Type: ' + pokemons.type + ', ' + 'Weaknesses: ' + pokemons.weaknesses[0] + '<br>');
-    console.log('Name: ' + pokemons.name + ', ' + 'Height: '  + pokemons.height + ', ' + 'Type: ' + pokemons.type + ', ' + 'Weaknesses: ' + pokemons.weaknesses[0] + '<br>');
+pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon)
 });
-
